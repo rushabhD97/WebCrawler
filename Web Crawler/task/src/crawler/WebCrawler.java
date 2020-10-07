d11df7a2dae1b2cd7e2bb2ccb7acc9cb5a9d7046
Rushabh Doshi <rushabhdoshi.97@gmail.com>
2020-10-07 20:06:37 +0530

    Captures Source code of given url

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -1,21 +1,52 @@
 package crawler;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class WebCrawler extends JFrame {
     public WebCrawler() {
+        super("Web Crawler");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setVisible(true);
-        setTitle("Simple Window");
+        setTitle("Web Crawler");
         initComponents();
     }
 
     private void initComponents() {
-        JTextArea jTextArea = new JTextArea();
-        jTextArea.setName("TextArea");
-        jTextArea.setText("HTML code?");
-        jTextArea.setEnabled(false);
-        add(jTextArea);
+        JTextArea htmlTextArea = new JTextArea();
+        htmlTextArea.setName("HtmlTextArea");
+        htmlTextArea.setEnabled(false);
+
+        JTextField urlTextField = new JTextField();
+        urlTextField.setBounds(20,20,100,20);
+        urlTextField.setName("UrlTextField");
+
+        JButton runButton = new JButton("Run");
+        runButton.setName("RunButton");
+        runButton.setBounds(150,20,50,20);
+        ActionListener runActionListener = e -> {
+            final String url = urlTextField.getText();
+            try(
+
+                    InputStream is = new BufferedInputStream(new URL(url).openStream())
+            ){
+                String siteSourceCode = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+                htmlTextArea.setText(siteSourceCode);
+                System.out.println(url + "\n" + siteSourceCode);
+            }catch (IOException io){
+                io.printStackTrace();
+            }
+        };
+        runButton.addActionListener(runActionListener);
+
+        add(htmlTextArea);
+        add(urlTextField);
+        add(runButton);
     }
 }
