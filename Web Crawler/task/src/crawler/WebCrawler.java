797e3b5393ce764c60b78079f71981f0dcd22046
Rushabh Doshi <rushabhdoshi.97@gmail.com>
2020-10-08 02:15:03 +0530

    Parsing title of the webpage

--- Web Crawler/task/src/crawler/WebCrawler.java
+++ Web Crawler/task/src/crawler/WebCrawler.java
@@ -1,12 +1,15 @@
 package crawler;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebCrawler extends JFrame {
     public WebCrawler() {
@@ -24,12 +27,13 @@ public class WebCrawler extends JFrame {
         htmlTextArea.setEnabled(false);
 
         JTextField urlTextField = new JTextField();
-        urlTextField.setBounds(20,20,100,20);
         urlTextField.setName("UrlTextField");
-
-        JButton runButton = new JButton("Run");
+        JButton runButton = new JButton("Parse");
         runButton.setName("RunButton");
-        runButton.setBounds(150,20,50,20);
+
+        JLabel titleLabel = new JLabel("Title: ");
+        titleLabel.setName("TitleLabel");
+
         ActionListener runActionListener = e -> {
             final String url = urlTextField.getText();
             try(
@@ -38,15 +42,32 @@ public class WebCrawler extends JFrame {
             ){
                 String siteSourceCode = new String(is.readAllBytes(), StandardCharsets.UTF_8);
                 htmlTextArea.setText(siteSourceCode);
+
+                titleLabel.setText(getTitle(siteSourceCode));
                 System.out.println(url + "\n" + siteSourceCode);
             }catch (IOException io){
                 io.printStackTrace();
             }
         };
         runButton.addActionListener(runActionListener);
+        setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
 
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BoxLayout(topPanel,BoxLayout.X_AXIS));
+        topPanel.add(urlTextField);
+        topPanel.add(runButton);
+        add(topPanel);
+        add(titleLabel);
         add(htmlTextArea);
-        add(urlTextField);
-        add(runButton);
+
+    }
+
+    private String getTitle(String siteSourceCode) {
+        Pattern pattern = Pattern.compile("(<title>)(.*)(<\\/title>)");
+        Matcher matcher = pattern.matcher(siteSourceCode);
+        if(matcher.find()){
+            return matcher.group(2);
+        }
+        return "No Title Tag";
     }
 }
